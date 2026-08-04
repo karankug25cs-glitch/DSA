31d0349dea0e105bb220c4194856eacba764ac03
karank <karank.ug25.cs@nitp.ac.in>
2026-08-04 23:57:21 +0530

    Time: 1 ms (77.09%) | Memory: 46.9 MB (24.94%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] arr) {
+        int n = arr.length;
+        int k = 1;
+        for(int i=1 ; i<n ; i++){
+            if(arr[i] != arr[i-1]){
+                arr[k] = arr[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
