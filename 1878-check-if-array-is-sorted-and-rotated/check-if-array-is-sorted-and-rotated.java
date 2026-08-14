d2d26fd2decfba404531aa684cefcb1a8728aa00
karank <karank.ug25.cs@nitp.ac.in>
2026-08-14 12:16:58 +0530

    Time: 9 ms (1.87%) | Memory: 44.9 MB (12.16%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,15 +1,31 @@
+import java.util.*;
 class Solution {
     public boolean check(int[] nums) {
-        int turn= 0;
-        for(int i=0; i<nums.length-1; i++){
-            if(nums[i] > nums[i+1])
-                turn++;
+        // int turn= 0;
+        // for(int i=0; i<nums.length-1; i++){
+        //     if(nums[i] > nums[i+1])
+        //         turn++;
+        // }
+        // if(nums[nums.length -1] > nums[0])
+        //         turn++;
+        // if(turn <= 1){
+        //     return true;
+        // }
+        // else return false;
+
+        String s ="";
+        for(int i : nums){
+            s += i+" ";
         }
-        if(nums[nums.length -1] > nums[0])
-                turn++;
-        if(turn <= 1){
-            return true;
+        String sp = s+s;
+        int[] sorted = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(sorted);
+        String sort = "";
+        for(int j : sorted){
+            sort += j + " ";
         }
-        else return false;
+        if(sp.contains(sort)) return true;
+        return false;
+
     }
 }
